ab0fa2538144d187b87e212935ff5b981c9f75f9
SebastianGonzalezMorales <sebastian.gonzalez@alumnos.uv.cl>
2020-11-20 20:13:55 -0300

    Tarea 01

--- OUILookup.py
+++ OUILookup.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 
-# Script que genera tiempos aleatorios de llegada de paquetes con
-# una tasa de arribo --rate, entre 0 y --maxtime segundos.
+# Script que verifica si la ip ingresada corresponde a la misma red del host
+# del usuario que ejecuta el script, también dada una mac address, revisa si
+# existe en el archivo utilizado como base de datos 
+# (https://gitlab.com/wireshark/wireshark/-/raw/master/manuf) 
+# y muestra el vendedor de ella.
 
 # Modo de uso:
 #
-# Uso: ./random-test.py --rate <rate>  --maxtime <maximum time> [--help] 
-#
-# Parametros:
-#     --rate: tasa de llegada. Obligatorio
-#     --maxtime: tiempo maximo de simulacion. Obligatorio
-#     --help: muestra esta pantalla y termina. Opcional
-     
+## python3 OUILookup.py
+
+#Use: ./python3 OUILookup.py --ip <IP<IP>> | --mac <MAC<MAC>> [--help]
+    
+#    --ip : specify the IP of the host to query.
+#	--mac: specify the MAC address to query. P.e. aa:bb:cc:00:00:00.
+#	--help: show this message and quit.
 
 import random
 import getopt
@@ -20,6 +23,7 @@ import requests
 import re
 import ipaddress
 import socket
+from getmac import get_mac_address
 
 #Cuerpo principal
 def main():
@@ -34,7 +38,6 @@ def main():
     if len(sys.argv) != 3:
         uso()
 
-    
     IP = None
     MAC  = None
     
@@ -46,43 +49,40 @@ def main():
         elif opt in ('--mac'):
             mac()
             
-
-    
     if IP != None and MAC != None:
             print('\n'"Error: Faltan parametros obligatorios.")
             uso()
 
-#
-#
-#
+# Función modo de uso
 def uso():
-    print('\n' "Use: " + sys.argv[0] + " --ip <IP> | --mac <MAC> [--help] ")
+    print('\n' "Use: python3" + sys.argv[0] + " --ip <IP> | --mac <MAC> [--help] ")
     print("\nParametros:")
     print("     --ip: specify the IP of the host to query.")
     print("     --mac: specify the MAC address to query. P.e. aa:bb:cc:00:00:00.")
     print("     --help: show this message and quit." '\n')
     exit(1)
 
+# Función que verifica si la ip ingresada pertenece a la misma red del host, tambíen muestra 
+# mac addres del host
 def ip():
 
     try:
         ip = ipaddress.ip_address(sys.argv[2])
-        print('\n%s is a correct IP%s address.' % (ip, ip.version))
+        print('\n%s is a correct IP%s address.\n' % (ip, ip.version))
     except ValueError:
-        print('ip incorrecta')
+        print('\nIp incorrect')
         uso()
     except:
         print('Usage : %s  ip' % sys.argv[0])
-        #print('\n' "Ip es: " + sys.argv[2] + '\n')
     
     nombre_equipo = socket.gethostname()
     direccion_equipo = socket.gethostbyname(nombre_equipo)
 
-    print ('\nLa IP es: %s' %direccion_equipo)
+    #print ('\nIP of host is: %s' %direccion_equipo)
     
     direccionip = direccion_equipo
     direccionip = direccionip.split('.')
-    
+     
     primerOcteto = int(direccionip[0])
     segundoOcteto = int(direccionip[1])
     tercerOcteto = int(direccionip[2])
@@ -96,27 +96,29 @@ def ip():
     tOcteto1 = int(direccionip2[2])
     cOcteto1 = int(direccionip2[3])
     
+    ip_mac = get_mac_address(str(direccionip))
+
     if(primerOcteto >= 1 and primerOcteto <=127	):
-        print("\nGiven IP address belongs to Class A")
+        #print("\nGiven IP address belongs to Class A")
         if(primerOcteto == pOcteto1):
-                print("")
-                print("Pertenecen a la misma red\n")
+            print("MAC address : " + ip_mac)
+            print("Ip is inside the host network\n")
         else:
             print("Error: ip is outside the host network\n")
     
     elif(primerOcteto >= 128 and primerOcteto <=191):
-        print("\nGiven IP address belongs to Class B")
+        #print("\nGiven IP address belongs to Class B")
         if(primerOcteto == pOcteto1 and segundoOcteto == sOcteto1):
-            print("")
-            print("Pertenecen a la misma red\n")
+            print("MAC address : " + ip_mac)
+            print("Ip is inside the host network\n")
         else:
             print("Error: ip is outside the host network\n")
     
     elif(primerOcteto >= 192 and primerOcteto <= 223):
-        print("\nGiven IP address belongs to Class C")
+        #print("\nGiven IP address belongs to Class C")
         if(primerOcteto == pOcteto1 and segundoOcteto == sOcteto1 and tercerOcteto == tOcteto1):
-            print("")
-            print("Pertenecen a la misma red")
+            print("MAC address : " + ip_mac)
+            print("Ip is inside the host network\n")
         else:
             print("Error: ip is outside the host network\n")
     else:
@@ -125,66 +127,46 @@ def ip():
 
     exit(1)    
 
+# Función que revisa que la mac addres ingresada está en la base de datos, en caso de estar
+# muestra el vendedor
 def mac():
 
     url = 'https://gitlab.com/wireshark/wireshark/-/raw/master/manuf'
 
     res = requests.get(url)
-
     output = res.text
     inputvalue = (sys.argv[2])
-
-    print("\nMAC address : " + inputvalue)
-
-    #print(output)
 
     file = open("output.txt", "w")
     file.write(output)
 
-    #print(output)
-
     file = open('output.txt',"r") 
     found = False
-    for line in file:  #iterate over the file one line at a time(memory efficient)
-        if re.search(format(inputvalue),line):    #if string found is in current line then print it
-             
-            line = line.split("\t")
+    for line in file:  # itera sobre el archivo una línea a la vez
+        if re.search(format(inputvalue+"\t"),line):    # si la cadena encontrada está en la línea actual
+                                                       # Divide la cadena por el separador especificado,
+                                                       # (en este caso un tabulador "\t") en una arreglo
+                                                       #  donde cada palabra 
+                                                       # es un elemento. 
+            line = line.split("\t") 
             line1 = len(line)
-
-            if line1 == 3:
+            
+            if inputvalue == line[1]:
+                uso()
+            elif line1 == 4:
+                print("\nMAC address : " + inputvalue)
+                print("\nVendor      : " + line[3])
+            elif line1 == 3:
+                print("\nMAC address : " + inputvalue)
                 print("Vendor      : " + line[2])
             elif line1 == 2:
-                print("Vendor      : " + line[1])
+                print("\nMAC address : " + inputvalue)
+                print("\nVendor      : " + line[1])
 
             found = True
                
     if not found:
-        print("Vendor      : Not found\n")
-    
-    
-        
-#
-# Genera Eventos
-# Entrada: 
-#          rate: tasa de arribo de paquetes.
-#          maxTime: tiempo maximo de generacion.
-# Salida:
-#          eventos : lista que contiene los tiempos de los eventos generados.
-#
-
-#def genEvents(rate, maxTime, eventos):
-    #t=0
-    #while t < maxTime:
-      #  dt = random.expovariate(1.0 / rate)
-     #   dt = (dt, 1)[dt == 0]
-
-    #    t = (t + dt, maxTime)[t+dt > maxTime]
-   #     eventos.append(t)
- 
-#def showEvents(ev):
- #   for fooEv in ev:
-  #      print(fooEv)
-
+        print("\nVendor      : Not found\n")
 
 #Esto se utiliza para poder importar este codigo en otro script para utilizar sus funciones.
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -6,9 +6,9 @@
 
 Ejemplo de uso sin parámetros o con la opción --help.
 
-**./OUILookup**
+**python3 OUILookup.py**
 
-Use: ./OUILookup --ip <IP<IP>> | --mac <MAC<MAC>> [--help]
+Use: python3 OUILookup.py --ip <IP<IP>> | --mac <MAC<MAC>> [--help]
     
     --ip : specify the IP of the host to query.
 	--mac: specify the MAC address to query. P.e. aa:bb:cc:00:00:00.
@@ -18,25 +18,25 @@ Use: ./OUILookup --ip <IP<IP>> | --mac <MAC<MAC>> [--help]
 
 ***Caso ip que pertenezca a su misma red***
 
-**./OUILookup --ip 192.168.1.5**
+**python3 OUILookup.py --ip 192.168.1.5**
 
     MAC address : b4:b5:fe:92:ff:c5
     Vendor      : Hewlett Packard
 
 ***Caso ip que NO pertenezca a su misma red***
 
-**./OUILookup --ip 192.168.10.5**
+**python3 OUILookup.py --ip 192.168.10.5**
 
     Error: ip is outside the host network
     Caso MAC que esté en la base de datos
 
-**./OUILookup --mac 98:06:3c:92:ff:c5**
+**python3 OUILookup.py --mac 98:06:3c:92:ff:c5**
 
     MAC address : 98:06:3c:92:ff:c5
     Vendor      : Samsung Electronics Co.,Ltd
     Caso MAC que no esté en la base de datos
 
-**./OUILookup --mac 98:06:3f:92:ff:c5**
+**python3 OUILookup.py --mac 98:06:3f:92:ff:c5**
     
     MAC address : 98:06:3f:92:ff:c5
     Vendor      : Not found
